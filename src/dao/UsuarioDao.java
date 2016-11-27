3ef3c4b396bedbfde68b23edd2d29c9f832f306c
KEVIN DANIEL PINCHI GARCIA <fliper1960.2@gmail.com>
2016-11-27 01:58:22 -0500

    mejoras2

---
+++ src/dao/UsuarioDao.java
@@ -0,0 +1,97 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+import database.DataAccess;
+import entity.Usuario;
+import interfaces.Intermetodos;
+
+public class UsuarioDao implements Intermetodos<Usuario> {
+
+	@Override
+	public Usuario Grabar(Usuario o) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Eliminar(Usuario o) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Modificar(Usuario o) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Usuario> Listar() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Usuario> Filtrar(Usuario o) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario Buscar(Usuario o) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Usuario Login(Usuario o) throws Exception {
+
+		Connection cn = null;
+		// boolean sw;
+
+		Usuario u = new Usuario();
+
+		try {
+			cn = DataAccess.getConnection();
+			String query = " select usuario_id, usuario, contrasena, estado from usuario where usuario = ? and "
+					+ " contrasena = ? and estado = 1 ";
+			PreparedStatement pstm = cn.prepareStatement(query);
+			pstm.setString(1, o.getUsuario());
+			pstm.setString(2, o.getContrasena());
+
+			ResultSet rs = pstm.executeQuery();
+
+			if (rs.next()) {
+
+				while (rs.next()) {
+
+					// u.setUsuario_id(rs.getInt("usuario_id"));
+					u.setUsuario(rs.getString("usuario"));
+					u.setEstado(rs.getInt("estado"));
+
+				}
+			} else {
+
+				u = null;
+			}
+
+			rs.close();
+			pstm.close();
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			try {
+				cn.close();
+			} catch (Exception exception) {
+				// TODO: handle exception
+			}
+		}
+		return u;
+
+	}
+
+}
---
+++ src/servlets/ServletUsuario.java
@@ -0,0 +1,87 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import dao.UsuarioDao;
+import entity.Usuario;
+
+/**
+ * Servlet implementation class ServletUsuario
+ */
+@WebServlet("/ServletUsuario")
+public class ServletUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletUsuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+
+		String opcion = request.getParameter("opcion");
+
+		String usuario;
+		String contrasena;
+
+		Usuario u;
+		UsuarioDao uDao;
+
+		if (opcion.equals("login")) {
+			usuario = request.getParameter("usuario");
+			contrasena = request.getParameter("contrasena");
+
+			try {
+
+				u = new Usuario();
+				u.setUsuario(usuario);
+				u.setContrasena(contrasena);
+
+				uDao = new UsuarioDao();
+
+				Usuario usu = uDao.Login(u);
+
+				String json = new Gson().toJson(usu);
+
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write(json);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
